2cbd1fd36a0f01264d80a407821cdf7d85a29e70
Harsh Agrawal <harsh.mobile231@gmail.com>
2024-03-14 10:11:39 +0530

    init commit

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,23 +1,15 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+// import './Tab1.css';
+import '../input.css'
+
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
+     <div className='bg-red-600 h-40 w-40'>
+      <h1>hey</h1>
+     </div>
     </IonPage>
   );
 };
