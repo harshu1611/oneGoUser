4b78483e0102ab2e4240f6435431c44ef04279c2
Kartik8Dwivedi <codewithme.kartik@gmail.com>
2024-03-14 22:15:44 +0530

    card component added

---
+++ src/components/Card.tsx
@@ -0,0 +1,54 @@
+import { FaTrain } from "react-icons/fa6";
+import { FaArrowRight , FaBus , FaCarAlt } from "react-icons/fa";
+import { FaLeaf } from "react-icons/fa";
+
+const Card = () => {
+  return (
+    <div className="w-[95vw] h-auto shadow-lg rounded-lg border-2">
+      <div className="flex items-center p-2 border-b-4 border-[#1656B7]">
+        <FaTrain color="green" className="w-10 h-6" />
+        <FaArrowRight color="#1656B7" className="w-6 h-5" />
+        <FaBus color="green" className="w-10 h-6" />
+        <FaArrowRight color="#1656B7" className="w-6 h-5" />
+        <FaCarAlt color="green" className="w-10 h-7" />
+      </div>
+      <div className="flex">
+        {/* mumbai/jaipur */}
+        <div className="w-[130vw] pt-1">
+          <div className="h-[10vh] flex justify-center">
+            <div className="text-black w-[45%] flex-col justify-center items-center gap-0">
+              <div className="text-center text-lg text-[#1656B7] font-semibold">
+                Mumbai
+              </div>
+              <div className="text-center font-semibold ">16:24</div>
+              <div className="text-center">16th Mar</div>
+            </div>
+            <FaArrowRight color="#1656B7" className="mt-2 h-4 w-[15%] pt-" />
+            <div className="text-black w-[45%] flex-col justify-center items-center gap-0">
+              <div className="text-center text-lg text-[#1656B7] font-semibold">
+                Jaipur
+              </div>
+              <div className="text-center font-semibold ">23:44</div>
+              <div className="text-center">17th Mar</div>
+            </div>
+          </div>
+        </div>
+        <div className="text-black w-full h-full flex-col justify-center items-center pt-2">
+          <div className="font-semibold text-xl text-center text-[#1656B7]">
+            {" "}
+            ₹2400/-
+          </div>
+          <div className="text-center text-lg font-medium">Total Fare</div>
+        </div>
+      </div>
+      <div className="text-black flex flex-row-reverse font-light text-sm relative tracking-tighter bottom-1 pb-1 pr-1 gap-2">
+        <div className="bg-green-500 flex rounded-lg">
+          <FaLeaf className="h-4 w-4" />
+          <div>Green Ride</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card
---
+++ src/pages/booking/Booking.tsx
@@ -0,0 +1,85 @@
+import { IonPage } from "@ionic/react";
+import React, { useEffect, useState } from "react";
+import { FaArrowAltCircleDown, FaArrowCircleDown } from "react-icons/fa";
+import TrainCard from "../../components/TrainCard";
+import supabase from "../../utils/supabase";
+import { trainDummy } from "../../dummy";
+import { key } from "ionicons/icons";
+import { useAppDispatch } from "../../redux/store";
+import { updateJourney } from "../../redux/states/User";
+import Card from "../../components/Card";
+
+const Booking: React.FC = () => {
+  const [startingMode, setStartingMode] = useState("Train");
+  const [trainFrom, setTrainFrom] = useState("");
+  const [trainTo, setTrainTo] = useState("");
+  const [trainDOJ, setTrainDOJ] = useState<any>();
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(updateJourney("rail2"));
+  }, []);
+
+  return (
+    <IonPage className="bg-white">
+      <div className="flex flex-col overflow-scroll bg-white">
+        <div className="flex flex-col bg-primaryBlue h-auto p-2">
+          <h1 className="text-white text-sm font-semibold">
+            Select Initial Mode
+          </h1>
+          <h1
+            className="text-white text-sm font-semibold"
+            onClick={async () => await supabase.auth.signOut()}
+          >
+            Log Out
+          </h1>
+        </div>
+        <form className="w-full h-full" onSubmit={(e) => e.preventDefault()}>
+          <div className="flex flex-col bg-white rounded-t-xl p-2 space-y-2">
+            <input
+              className="w-full h-auto p-2 border-primaryBlue border-[1px] rounded-lg bg-white text-gray-600"
+              title="From"
+              placeholder="From"
+              onChange={(e) => setTrainFrom(e.target.value)}
+              value={trainFrom}
+            ></input>
+            <FaArrowAltCircleDown
+              className="self-center"
+              color="#1656B7"
+              size={20}
+            />
+            <input
+              className="w-full h-auto p-2 border-primaryBlue border-[1px] rounded-lg bg-white text-gray-600"
+              title="To"
+              placeholder="To"
+              onChange={(e) => setTrainTo(e.target.value)}
+              value={trainTo}
+            ></input>
+            <input
+              className="w-full h-auto p-2 border-primaryBlue border-[1px] rounded-lg  bg-white text-gray-600"
+              placeholder="Date Of Journey"
+              type=""
+              onFocus={(e) => (e.target.type = "date")}
+              onChange={(e) => setTrainDOJ(e.target.value)}
+              value={trainDOJ}
+            ></input>
+            <button
+              className="w-32 h-10 bg-primaryBlue border-2 text-white rounded-lg p-1 self-center font-semibold"
+              onClick={() => {}}
+            >
+              Show Options
+            </button>
+          </div>
+        </form>
+        <div className="flex-col items-center gap-4">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    </IonPage>
+  );
+};
+
+export default Booking;
